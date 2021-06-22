45e92260b490e667d48967f200b0bf096bf7a3a4
Israel Acuña Uval <isra.acuna@duocuc.cl>
2021-06-22 05:04:17 -0400

    Diccionario de contexto con objetos

--- core/views.py
+++ core/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        super().__init__()
+
 # Create your views here.
 def home(request):
 
     lista_heroes = ['Batman', 'Captain America', 'Loki']
 
+    villano = Persona('Thanos', '45')
+
     contexto = {
         'nombre': 'Israel Acuña',
         'edad': 33,
-        'heroes':lista_heroes
+        'heroes':lista_heroes,
+        'villano': villano
     }
+
     return render(request,'core/home.html', contexto)
